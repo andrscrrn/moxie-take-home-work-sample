153857fa58bcd98053ae085f51b5e199c0cf2931
Andres Carreño <hello@andrescarreno.co>
2023-09-29 18:48:32 -0500

    Fixing page padding on large

--- src/app/booking/layout.tsx
+++ src/app/booking/layout.tsx
@@ -7,18 +7,13 @@ export default function BookingLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="px-4 lg:px-0 pb-28">
+    <div className="px-4 pb-28">
       <h1 className="text-center text-neutral-900 text-lg font-bold leading-normal tracking-tight my-3 lg:my-0 lg:hidden">
         Book appointment
       </h1>
       {children}
       <BottomBar>
-        <Button
-          className="w-full lg:w-auto"
-          label="Continue"
-          primary
-          size="large"
-        />
+        <Button className="w-full lg:w-auto" label="Continue" size="large" />
       </BottomBar>
     </div>
   )
